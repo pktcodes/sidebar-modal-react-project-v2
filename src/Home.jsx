51315d9679526d57ab06e4b197f351350bbe4eb1
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-08-14 23:30:33 -0500

    Global Context

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,10 @@
+import { useGlobalContext } from './context';
+
 const Home = () => {
+  const { openSidebar, openModal } = useGlobalContext();
+
+  console.log(openSidebar);
+
   return <h2>home</h2>;
 };
 
