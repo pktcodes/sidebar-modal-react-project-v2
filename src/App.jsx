08749add674c8d9e4969854644cf3c43811f1f45
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-08-15 00:16:19 -0500

    Home Component

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import Sidebar from './Sidebar';
 
 const App = () => {
   return (
-    <main>
+    <>
       <Home />
       <Sidebar />
       <Modal />
-    </main>
+    </>
   );
 };
 export default App;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,22 @@
 import { useGlobalContext } from './context';
 
+import { FaBars } from 'react-icons/fa';
+
 const Home = () => {
   const { openSidebar, openModal } = useGlobalContext();
 
   console.log(openSidebar);
 
-  return <h2>home</h2>;
+  return (
+    <main>
+      <button type="button" className="sidebar-toggle" onClick={openSidebar}>
+        <FaBars />
+      </button>
+      <button type="button" className="btn" onClick={openModal}>
+        show modal
+      </button>
+    </main>
+  );
 };
 
 export default Home;
